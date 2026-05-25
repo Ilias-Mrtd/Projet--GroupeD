4dfc5eca30e6f226df2a830ba147df8bced480e2
Artess92 <adem@ben-halima.com>
2026-05-25 21:28:05 +0200

    feat: ajout du systeme de tick global dans SimulationEngine

--- src/SimulationEngine/SimulationEngine.java
+++ src/SimulationEngine/SimulationEngine.java
@@ -1,6 +1,7 @@
 package SimulationEngine;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import SimulationEngine.GraphRenderer.agents.Agent;
 import SimulationEngine.GraphRenderer.graph.Graph;
@@ -9,5 +10,22 @@ public class SimulationEngine {
 
     public Graph graph;
     public List<Agent> Agents;
+
+    public SimulationEngine(Graph graph) {
+        this.graph = graph;
+        this.Agents = new ArrayList<>();
+    }
+
+    public void addAgent(Agent agent) {
+        agent.graph = this.graph; 
+        this.Agents.add(agent);
+        System.out.println("Agent " + agent.id + " ajouté au moteur de simulation.");
+    }
+
+    public void tick() {
+        for (Agent agent : Agents) {
+            agent.update(); 
+        }
+    }
 
 }
