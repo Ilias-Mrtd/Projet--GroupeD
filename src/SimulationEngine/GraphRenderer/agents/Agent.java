f504ca26315324721ec2447577150f8df2718d6d
Artess92 <adem@ben-halima.com>
2026-05-25 20:21:42 +0200

    feat: implementation de la classe Agent et validation du MVP de simulation

--- src/SimulationEngine/GraphRenderer/agents/Agent.java
+++ src/SimulationEngine/GraphRenderer/agents/Agent.java
@@ -23,4 +23,29 @@ public class Agent extends Dijkstra {
         WAITING
     }
 
+    public Agent(String id, float speed, String state) {
+
+    super(new Graph(), null, null); 
+    this.id = Integer.parseInt(id);
+    this.speed = speed;
+    this.state = agentState.valueOf(state.toUpperCase());
+}
+
+    public void setDestination(Node dest) {
+        this.Destination = dest;
+        this.state = agentState.CALCULATING;
+        
+        if (this.graph != null && this.currentNode != null) {
+            findPath(this.graph, this.currentNode, this.Destination);
+        }
+        
+        this.state = agentState.RUNNING;
+    }
+
+    public void update() {
+        if (this.state == agentState.RUNNING) {
+            System.out.println("Agent " + this.id + " en mouvement vers " + this.Destination.id);
+        }
+    }
+
 }
